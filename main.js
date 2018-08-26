f9c789f8b28e28b88079d6945dc6521fd81dd3cb
kazygumi <kazygumi@gmail.com>
2018-08-26 18:10:09 +0900

    新規ファイル追加

---
+++ main.js
@@ -0,0 +1,17 @@
+(function ($) {
+    "use strict";
+
+    function locationHashChanged() {}
+
+    function resize(e) {}
+
+    window.addEventListener('DOMContentLoaded', function () {
+
+        window.onhashchange = locationHashChanged;
+
+        window.addEventListener('resize', resize, false);
+        resize(null);
+
+    }, false);
+
+})(jQuery);
