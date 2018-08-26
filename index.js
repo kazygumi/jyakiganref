6d9f5e43f6f9de25cc9f2d343d24921c01711143
kazygumi <kazygumi@gmail.com>
2018-08-26 22:01:30 +0900

    名前変更

--- index.js
+++ index.js
@@ -6,12 +6,9 @@
     function resize(e) {}
 
     window.addEventListener('DOMContentLoaded', function () {
-
         window.onhashchange = locationHashChanged;
-
         window.addEventListener('resize', resize, false);
         resize(null);
-
     }, false);
 
 })(jQuery);
