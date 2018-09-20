406e7b6dfabd83d51f0cce8cd2967b40320a8f71
kazygumi <kazygumi@gmail.com>
2018-09-20 21:42:01 +0900

    デコレーションの乱数を奇数と偶数で出し分けし、バランスよくした

--- public/textDecorator.js
+++ public/textDecorator.js
@@ -1,19 +1,27 @@
 (() => {
-	'use strict';
+    'use strict';
 
-	function textDecorate(collection, num) {
-		for (let i = 0; i < collection.length; i++) {
-			const h1Elm = collection.item(i);
-			const h1Text = h1Elm.textContent;
-			h1Elm.innerHTML = null;
+    function textDecorate(collection, num) {
+        for (let i = 0; i < collection.length; i++) {
+            const h1Elm = collection.item(i);
+            const h1Text = h1Elm.textContent;
+            h1Elm.innerHTML = null;
+            let count = 0;
+            h1Text.split('').forEach(function (c) {
+                let classNum = 0;
+                if ((count % 2) == 0) {
+                    classNum = Math.floor(Math.random() * Math.floor(num / 2)) * 2;
+                } else {
+                    classNum = Math.floor(Math.random() * Math.floor(num / 2)) * 2 + 1;
+                }
+                console.log(count+ " " +classNum);
+                
+                h1Elm.innerHTML += '<span class="deco' + classNum + '">' + c + '</span>';
+                count++;
+            });
+        }
+    }
 
-			h1Text.split('').forEach(function (c) {
-				const classNum = Math.floor(Math.random() * num);
-				h1Elm.innerHTML += '<span class="deco' + classNum + '">' + c + '</span>';
-			});
-		}
-	}
-	
-	textDecorate(document.getElementsByTagName('h1'), 4);
+    textDecorate(document.getElementsByTagName('h1'), 4);
 
 })();
