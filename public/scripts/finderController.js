0ccbfc0ba8ef7abd0934110d79605f1129091ae7
kazygumi <kazygumi@gmail.com>
2018-10-12 06:18:59 +0900

    表情グラフを復活

--- public/scripts/finderController.js
+++ public/scripts/finderController.js
@@ -185,6 +185,131 @@
 
         const cp = ctrack.getCurrentParameters();
 
+        const er = ec.meanPredict(cp);
+        if (er) {
+            updateData(er);
+            for (let i = 0; i < er.length; i++) {
+                if (er[i].value > 0.4) {
+                    document.getElementById('emotion_icon' + (i + 1)).style.visibility = 'visible';
+                } else {
+                    document.getElementById('emotion_icon' + (i + 1)).style.visibility = 'hidden';
+                }
+            }
+        }
+
+    }
+
+    delete emotionModel['disgusted'];
+    delete emotionModel['fear'];
+    const ec = new emotionClassifier();
+    ec.init(emotionModel);
+    const emotionData = ec.getBlank();
+
+    /************ d3 code for barchart *****************/
+
+    
+    
+    const margin = {
+            top: 20,
+            right: 20,
+            bottom: 10,
+            left: 40
+        },
+        width = 400 - margin.left - margin.right,
+        height = 100 - margin.top - margin.bottom;
+
+    const barWidth = 30;
+
+    const formatPercent = d3.format(".0%");
+
+    const x = d3.scale.linear()
+        .domain([0, ec.getEmotions().length]).range([margin.left, width + margin.left]);
+
+    const y = d3.scale.linear()
+        .domain([0, 1]).range([0, height]);
+
+    const svg = d3.select("#emotion_chart").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+
+    svg.selectAll("rect").
+    data(emotionData).
+    enter().
+    append("svg:rect").
+    attr("x", function (datum, index) {
+        return x(index);
+    }).
+    attr("y", function (datum) {
+        return height - y(datum.value);
+    }).
+    attr("height", function (datum) {
+        return y(datum.value);
+    }).
+    attr("width", barWidth).
+    attr("fill", "#666666");
+
+    svg.selectAll("text.labels").
+    data(emotionData).
+    enter().
+    append("svg:text").
+    attr("x", function (datum, index) {
+        return x(index) + barWidth;
+    }).
+    attr("y", function (datum) {
+        return height - y(datum.value);
+    }).
+    attr("dx", -barWidth / 2).
+    attr("dy", "1.2em").
+    attr("text-anchor", "middle").
+    text(function (datum) {
+        return datum.value;
+    }).
+    attr("fill", "white").
+    attr("class", "labels");
+
+    svg.selectAll("text.yAxis").
+    data(emotionData).
+    enter().append("svg:text").
+    attr("x", function (datum, index) {
+        return x(index) + barWidth;
+    }).
+    attr("y", height).
+    attr("dx", -barWidth / 2).
+    attr("text-anchor", "middle").
+    attr("style", "font-size: 12").
+    attr("fill", "white").
+    text(function (datum) {
+        return datum.emotion;
+    }).
+    attr("transform", "translate(0, 18)").
+    attr("class", "yAxis");
+
+    function updateData(data) {
+        // update
+        const rects = svg.selectAll("rect")
+            .data(data)
+            .attr("y", function (datum) {
+                return height - y(datum.value);
+            })
+            .attr("height", function (datum) {
+                return y(datum.value);
+            });
+        const texts = svg.selectAll("text.labels")
+            .data(data)
+            .attr("y", function (datum) {
+                return height - y(datum.value);
+            })
+            .text(function (datum) {
+                return datum.value.toFixed(1);
+            });
+
+        // enter
+        rects.enter().append("svg:rect");
+        texts.enter().append("svg:text");
+
+        // exit
+        rects.exit().remove();
+        texts.exit().remove();
     }
 
     function locationHashChanged() {
